e28695de10d4ad87ae5c4d01eb830c62bf78ccb8
Ivan V <ivanntonio@gmail.com>
2024-08-06 21:39:09 -0600

    Fix-deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "build", // Cambia el nombre de la carpeta de salida a 'build'
+  },
   base: "/web-panikasos/",
 });
