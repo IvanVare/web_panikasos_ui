0ab5cf661dd1109e3310908a81b0569065819aa7
Ivan V <ivanntonio@gmail.com>
2025-01-29 14:35:37 -0600

    fix4- connect to backend

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "//ec2-3-82-247-114.compute-1.amazonaws.com:3331",
+  baseURL: "http//ec2-3-82-247-114.compute-1.amazonaws.com:3331",
 });
