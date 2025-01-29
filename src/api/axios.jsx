b61df2725aa7505cb5d8ae0b9e10ff8ee93b9824
Ivan V <ivanntonio@gmail.com>
2025-01-29 15:37:48 -0600

    fix8- connect to backend

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL:
-    "https://cors-anywhere.herokuapp.com/http://ec2-3-82-247-114.compute-1.amazonaws.com:333",
+  baseURL: "https://ec2-3-82-247-114.compute-1.amazonaws.com:3331",
 });
